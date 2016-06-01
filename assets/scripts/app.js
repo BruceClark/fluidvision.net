a1b995f3f6ef9439f6b21c2c704a31a4105d503b
Bruce Clark <bruce@fluidvision.net>
2016-06-01 05:09:17 -0600

    Big update: breakpoints, responsive js, structure
    Redid the whole responsive approach using a new medium breakpoint for 5s size devices. Also remapped the media queries to use Foundation's @include breakpoint methods.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 // List of Elements to Work With
 var elements = {
-  $personal_links:  $('#intro .links a')
+  $personal_links:  $('#intro .links a'),
+  $work_pieces:     $('#work .portfolio-wrap .piece')
 }
 
 // Page Updates (Mostly for Responsive)
@@ -13,11 +14,30 @@ var site = {
     site_width = $(window).width();
 
     // Change button class on #intro
-    if (site_width < 640) {
+    if (site_width < 668) {
       elements.$personal_links.removeClass('secondary');
+      site.highlight_scroll();
     } else {
       elements.$personal_links.addClass('secondary');
     }
+  },
+
+  highlight_scroll: function(){
+    elements.$work_pieces.each(function(index, el) {
+      var $self = $(this);
+      var $target = $(this).children('a');
+      var element_top = $self.offset().top - 260;
+
+      $(window).scroll(function(event) {
+        var scrolled = $(window).scrollTop();
+
+        if (scrolled > element_top) {
+          $target.addClass('scrolled');
+        } else {
+          $target.removeClass('scrolled');
+        }
+      });
+    });
   }
 }
 
